92d33afd0b3e0b7e1d405c723dcad832fd154e78
aoimaru <51444995+aoimaru@users.noreply.github.com>
2021-02-24 17:01:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,54 @@
-import sys
+from flask import Flask, request, abort
 
-def test() -> None:
-    print("test")
-    pass
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('')
+handler = WebhookHandler('')
 
 
-def main() -> None:
-    test()
+@app.route("/")
+def test() -> str:
+    return "OK"
+
+
+
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=f"碧海さんは,{event.message.text}と言いました."))
 
 
 if __name__ == "__main__":
-    main()
+    app.run()
